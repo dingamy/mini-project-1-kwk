dfb7a965c288fe0dcbd34dabf9b4b9304c5f102c
Amy Ding <amyding04@gmail.com>
2022-07-19 13:55:00 -0500

    kwk project 1 - about me app

--- mini project 1 kwk/ViewController.swift
+++ mini project 1 kwk/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pressText: UILabel!
+    @IBOutlet weak var pianoPhoto: UIImageView!
+    @IBOutlet weak var hiAmy: UILabel!
+    @IBOutlet weak var smileText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressMe(_ sender: UIButton) {
+        pressText.text = "I'm from Winnipeg, Canada"
+        smileText.text = "I enjoy going on runs, playing piano, and card games!"
+//
+        
+    }
+    
 }
 
